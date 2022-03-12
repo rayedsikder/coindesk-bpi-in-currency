467993ff48d466295ecb20b0f047322498149196
Mohammad Rayed <mohammad.rayed@northsouth.edu>
2022-03-13 04:20:03 +0600

    Add functions to fetch data from CoindeskAPI and configure routes

--- routes/getBitcoinInfo.js
+++ routes/getBitcoinInfo.js
@@ -27,13 +27,38 @@ function checkCurrencyParameter(currencyCode) {
     })
 }
 
+function getBitcoinPriceForCurrency(currencyCode) {
+    const data = fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currencyCode}.json`)
+    const jsonObj = data.json("")
+    const rateFloat = jsonObj.bpi[currencyCode.toUpperCase()].rate_float;
+    return rateFloat;
+}
 
+function getMinMaxPrice(currencyCode) {
+    const data = fetch(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${currentDate.format("YYYY-MM-DD")}&end=${priorDate.format("YYYY-MM-DD")}&currency=${currencyCode}`);
+    const jsonObj = data.json();
+    let bpiValues = [];
+    for (const property in jsonObj.bpi) {
+        bpiValues.push(jsonObj.bpi[property]);
+      }
+    const maxBPI = Math.max(...bpiValues);
+    const minBPI = Math.min(...bpiValues);
+    return maxBPI, minBPI;
+}
 
 router.get("/", (req, res) => {
-    const currencyCode = req.body.currency;
+    const currencyCode = req.body;
     checkCurrencyParameter(currencyCode).then((message) => {
+        const price = getBitcoinPriceForCurrency(currencyCode);
+        const maxMinPrice = getMinMaxPrice(currencyCode);
+        const responseData = {
+            currency: currencyCode.toUpperCase(),
+            bpi: price,
+            maximumBPI: maxMinPrice[0],
+            minimumBPI: maxMinPrice[1]
+        }
         console.log("Success" + message);
-        res.send("data")
+        res.send(responseData);
     }).catch((error) => {
         res.status(400);
         res.send("error: " + error.message)
