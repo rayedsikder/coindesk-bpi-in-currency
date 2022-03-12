e87af2a5ca970796f2c5ae782fd6ea0dd45b7e48
Mohammad Rayed <mohammad.rayed@northsouth.edu>
2022-03-12 22:31:02 +0600

    build: Add dependencies and setup configs

---
+++ index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}`);
+});
