0fc8ca70d03f16cc3ca30e93a5a7b1ed3e7f31d5
Mohammad Rayed <mohammad.rayed@northsouth.edu>
2022-03-13 06:09:25 +0600

    Add tests to check if BPI is correctly returned

--- tests.js
+++ tests.js
@@ -1,11 +1,39 @@
 const chai = window.chai;
 const expect = chai.expect;
+import fetch from "node-fetch";
 
 describe ("getBitcoinPriceForCurrency", () => {
-    it("should return the BPI for the given currency", () => {
-        const price = await getBitcoinPriceForCurrency("USD");
+    it("should return the BPI for the USD", () => {
+        const currency = "USD";
         console.log("Current BPI: " + price);
-        assert(price != 0);
-        )
+        const data = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`)
+        const jsonObj = await data.json();
+        const rateFloat = jsonObj.bpi[currency.toUpperCase()].rate_float;
+        expect(getBitcoinPriceForCurrency(currency).to.deep.equal(rateFloat));
     })
+
+    it("should return the BPI for the GBP", () => {
+        const currency = "GBP";
+        const data = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`)
+        const jsonObj = await data.json();
+        const rateFloat = jsonObj.bpi[currency.toUpperCase()].rate_float;
+        expect(getBitcoinPriceForCurrency(currency).to.deep.equal(rateFloat));
+    })
+
+    it("should return the BPI for the EUR", () => {
+        const currency = "EUR";
+        const data = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`)
+        const jsonObj = await data.json();
+        const rateFloat = jsonObj.bpi[currency.toUpperCase()].rate_float;
+        expect(getBitcoinPriceForCurrency(currency).to.deep.equal(rateFloat));
+    })    
+
+
+    it("should throw error when parameters are empty", () => {
+        const currency = "";
+        const data = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`)
+        assert(1);
+    })      
+
 })
+
