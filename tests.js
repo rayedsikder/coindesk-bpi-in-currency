139c83e8f9155ee331bafac281983ea6bb1c4d04
Mohammad Rayed <mohammad.rayed@northsouth.edu>
2022-03-13 06:25:57 +0600

    Add tests to check the happy path for MaxMin BPI

--- tests.js
+++ tests.js
@@ -28,7 +28,6 @@ describe ("getBitcoinPriceForCurrency", () => {
         expect(getBitcoinPriceForCurrency(currency).to.deep.equal(rateFloat));
     })    
 
-
     it("should throw error when parameters are empty", () => {
         const currency = "";
         const data = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`)
@@ -37,3 +36,34 @@ describe ("getBitcoinPriceForCurrency", () => {
 
 })
 
+describe ("getMinMaxPrice", () => {
+    it("should return the maximum BPI within last month", () => {
+        const currency = "USD";
+        cosnt priorDate = "2021-02-01";
+        const currentDate = "2021-03-01";
+        const data = await fetch(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${priorDate}&end=${currentDate}&currency=${currency}`);
+        const jsonObj = await data.json();
+        let bpiValues = [];
+        for (const property in jsonObj.bpi) {
+            bpiValues.push(jsonObj.bpi[property]);
+        }
+        const maxBPI = Math.max(...bpiValues);
+        const functionMax = await getMinMaxPrice(currency);
+        expect(functionMax[0].to.deep.equal(maxBPI));
+    })
+
+    it("should return the minimum BPI within last month", () => {
+        const currency = "USD";
+        cosnt priorDate = "2021-02-01";
+        const currentDate = "2021-03-01";
+        const data = await fetch(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${priorDate}&end=${currentDate}&currency=${currency}`);
+        const jsonObj = await data.json();
+        let bpiValues = [];
+        for (const property in jsonObj.bpi) {
+            bpiValues.push(jsonObj.bpi[property]);
+        }
+        const minBPI = Math.max(...bpiValues);
+        const functionMin = await getMinMaxPrice(currency);
+        expect(functionMin[1].to.deep.equal(minBPI));
+    })    
+})
